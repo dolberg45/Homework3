6d2fbfc87b2a062d44efd6ff4b1675be774543e3
dolberg45 <dolberg4500@gmail.com>
2020-11-06 17:51:09 +0300

    Initial Commit

---
+++ Homework3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework3
+//
+//  Created by Григорий on 06.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
